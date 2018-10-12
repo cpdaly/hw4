95da01765532f1fe5506c4eae7750d507ae9019e
cpdaly <43274822+cpdaly@users.noreply.github.com>
2018-10-12 10:40:41 -0700

    Create cone-of-lines.js

---
+++ cone-of-lines.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(250);
+  
+  for(var x = 10; x<=390;x=x+10){
+  line(200,0,x,390)
+  //line(200,0, 390, 390)
+  //line(200,0, 10, 390)
+  //line(200,0,20,390)
+}
+}
